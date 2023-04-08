f9a6bedefe6d8b87d74be6f31027d3dc6bbf8890
Manidoux41 <lherissondu41@gmail.com>
2023-04-08 10:50:11 +0200

    finalisation carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import leftArrow from '../../assets/left-arrow.svg'
 import rightArrow from '../../assets/right-arrow.svg'
 
 function Carousel({picProps}) {
     console.log(picProps)
+
+    const [indexPic, setIndexPic] = useState(0)
+
+    const incrementDown = () => {
+      setIndexPic((indexPic + picProps.length - 1) % picProps.length)
+    }
+
+    const incrementUp = () => {
+      setIndexPic((indexPic + 1) % picProps.length)
+    }
+
   return (
     <div className='carousel'>
-      <img src={leftArrow} alt="" className="leftArrow" />
-        <img src={picProps[0]} alt="" />
-      <img src={rightArrow} alt="" className="rightArrow" />
+      <img src={leftArrow} alt="" className="leftArrow" onClick={incrementDown} />
+        <img src={picProps[indexPic]} alt="" />
+      <img src={rightArrow} alt="" className="rightArrow" onClick={incrementUp} />
       <p className="counter">
         1 / 3
       </p>
